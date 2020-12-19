ecd5641c9669d8b6e7b68fcd8d8e905b384c339f
Roy Sihotang <roynaldosihotang@gmail.com>
2020-12-19 22:36:56 +0700

    oop start code

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const name ='Roy'
-const nik = '2323133005940333'
-const bod = '1994-05-30'
-
-
-
 var personLogic = require('./domain/person/person.logic')
-var person1 = new personLogic(name,nik,bod);
+var person1 = new personLogic('Roy','2323133005940333','1994-05-30');
 person1.print();
-// console.log(employeeLogic)
+
 var personAge = require('./domain/personAge/personAge.logic')
-var age1 = new personAge (name,nik,bod);
+var age1 = new personAge ('Roy','2323133005940333','1994-05-30');
 age1.print();
 
 var validateNik = require('./domain/validateNik/validateNik.logic')
-var validate = new validateNik(name, nik, bod);
-validate.print();
+var validate1 = new validateNik('Roy','2323133005940333','1994-05-30');
+
+validate1.print();
+console.log('\n===================\n')
+
+var person2 = new personLogic('Dimas','2323133005940333','1980-05-30');
+person2.print();
+var age2 = new personAge ('Dimas','2323133005940333','1980-05-30');
+age2.print();
+var validate2 = new validateNik('Dimas','2323133005940333','1980-05-30');
+// console.log(employeeLogic)
+validate2.print();
+
+
+
+
